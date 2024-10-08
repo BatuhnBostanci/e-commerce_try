cffd69165297adeb9f24eb3eb9599c4e22094da8
Batuhan Bostancı <109556262+BatuhnBostanci@users.noreply.github.com>
2024-10-08 16:04:05 +0300

    Update BasketRepository.java

--- demo/src/main/java/com/example/demo/repository/BasketRepository.java
+++ demo/src/main/java/com/example/demo/repository/BasketRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BasketRepository extends JpaRepository<Basket,Integer> {
+public interface BasketRepository extends JpaRepository<Basket, Long> {
+    List<Basket> findByUserId(Long userId);
 }
